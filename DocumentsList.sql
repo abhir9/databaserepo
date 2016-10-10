99e88fb2d133f37d3ba21b03534d533432dc704f
Mobinext11 <kanchan.anthwal19@gmail.com>
2016-10-10 12:54:58 +0530

    inital commit

---
+++ DocumentsList.sql
@@ -0,0 +1,12 @@
+
+
+INSERT INTO votw.DocumentTypes (name) VALUES ("Passport");
+INSERT INTO votw.DocumentTypes (name) VALUES ("Residentail Proof");
+INSERT INTO votw.DocumentTypes (name) VALUES ("Aadhar Card");
+INSERT INTO votw.DocumentTypes (name) VALUES ("PAN Card");
+INSERT INTO votw.DocumentTypes (name) VALUES ("Electricity Bill");
+INSERT INTO votw.DocumentTypes (name) VALUES ("Telephone Bill");
+INSERT INTO votw.DocumentTypes (name) VALUES ("Driving License");
+INSERT INTO votw.DocumentTypes (name) VALUES ("Ration Card");
+INSERT INTO votw.DocumentTypes (name) VALUES ("Birth Certicifcate");
+INSERT INTO votw.DocumentTypes (name) VALUES ("Domicile Certificate");
--- UserTransactions.sql
+++ UserTransactions.sql
@@ -5,12 +5,14 @@ DROP TABLE IF EXISTS `UserTransactions`;
 
 CREATE TABLE `UserTransactions` (
    `userid` varchar(8) NOT NULL,
+`leadid` int(6) NOT NULL,
     `orderid` varchar(45) NOT NULL,
   `transactionid` varchar(45) NOT NULL,
   `amount` varchar(45) NOT NULL,
 `transactiondate` datetime NOT NULL,
 
- FOREIGN KEY (`userid`) REFERENCES `UserMaster` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+ FOREIGN KEY (`userid`) REFERENCES `UserMaster` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+FOREIGN KEY (`leadid`) REFERENCES `UserLeads` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
