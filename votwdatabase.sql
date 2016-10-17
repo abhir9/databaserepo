d01766a177d42b5986a1eb2aec4986c21698bf3c
Mobinext11 <kanchan.anthwal19@gmail.com>
2016-10-17 18:34:38 +0530

    Modified files

--- votwdatabase.sql
+++ votwdatabase.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `documenttypes`
 --
-
+CREATE DATABASE  IF NOT EXISTS `votw`;
+use 'votw';
 DROP TABLE IF EXISTS `documenttypes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
